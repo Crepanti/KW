d597d52b11426f9ea5ae858433527f5e1e4c3a7d
Crepanti <crepantidev@gmail.com>
2023-02-18 07:15:39 +0100

    Mega Update

--- scripts/assist.js
+++ scripts/assist.js
@@ -2,7 +2,6 @@ var scr = document.createElement("script");
 scr.type="text/javascript";
 scr.innerHTML = `
 $(".btn_small_gold.option[data-option='clan_assist']:visible").each( function() {
-  console.log($(this).data('tid'), $(this).data('target'))
 GAME.socket.emit('ga',{a:39,type:55,tid:$(this).data('tid'),target:$(this).data('target')})
 })
 `
--- scripts/wojny.js
+++
@@ -1,4 +0,0 @@
-let wojny = setInterval(() => {
-    if(parseInt($('#clan_war_cnt').text()) < 3 && GAME.server==19){
-GAME.emitOrder({a:39,type:24,shorts:"ALP"})};
- }, 60000);
