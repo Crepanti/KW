ddd0e3d3b173212d906208e7b055f939be51349b
Crepanti <crepantidev@gmail.com>
2023-03-08 15:48:42 +0100

    UP

--- content.js
+++ content.js
@@ -1,8 +1,17 @@
-$(document).bind('keydown', '0', function(){
-  if(JQS.chm.is(":focus") == false){
-    customLog(``)
-  }
-  return false;
+$(document).bind('keydown', '[', function(){
+  kom_clear();
+  var kom='<div class="quest_desc">&raquo; '+LNG.lab372+':<br /><div class="game_input"><input id="promo_code" type="text" value="miko2021" /></div><br /><br /><div id="promo_captcha" class="captcha_container"></div><br /><button class="option newBtn" data-option="promo_code_go">OK</button></div>';
+  GAME.komunikat(kom);
+  option_bind();
+  GAME.load_captcha('#promo_captcha','promo_captcha',0,1);
+});
+
+$(document).bind('keydown', ']', function(){
+  kom_clear();
+  var kom='<div class="quest_desc">&raquo; '+LNG.lab372+':<br /><div class="game_input"><input id="promo_code" type="text" value="kw19ep" /></div><br /><br /><div id="promo_captcha" class="captcha_container"></div><br /><button class="option newBtn" data-option="promo_code_go">OK</button></div>';
+  GAME.komunikat(kom);
+  option_bind();
+  GAME.load_captcha('#promo_captcha','promo_captcha',0,1);
 });
 
 GAME.emitOrder = (data) => GAME.socket.emit('ga',data);
